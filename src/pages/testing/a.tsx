57073cfbefdc87aef115cbf24e90f9a7679d0f68
Yena Hwang <hwangyena@sk.com>
2022-05-17 15:02:29 +0900

    [JS] 새 창, 새 탭 띄우기

---
+++ src/pages/testing/a.tsx
@@ -0,0 +1,7 @@
+export default function A() {
+  return (
+    <div>
+      <h1>New window</h1>
+    </div>
+  );
+}
--- src/pages/testing/index.tsx
+++ src/pages/testing/index.tsx
@@ -1,16 +1,29 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import Link from 'next/link';
 
 export default function TestingPage({ test }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const handleOpen = () => {
+    window.open('/testing/a', 'test', 'width=500, height=500');
+  };
+
   return (
     <div>
-      <h1>Test~!!</h1>
+      <h1>SSG TEST</h1>
       {test}
+      <button onClick={handleOpen} className="bg-slate-300 mr-5">
+        새로운 창 띄우기
+      </button>
+      <Link href="/testing/a" passHref>
+        <a target="_blank" className="bg-slate-300 p-2">
+          새 탭 띄우기
+        </a>
+      </Link>
     </div>
   );
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  console.log('param', params);
+  console.log('SSG Test', params);
   // const { router, EmployeeInvitation,InvitationError } = useCheckInvitation(context.params);
 
   return {

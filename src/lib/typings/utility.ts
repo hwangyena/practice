d36fd8bd92bdc2eef853663489386e4b2b69d741
Yena Hwang <hwangyena@sk.com>
2022-06-03 16:47:10 +0900

    [TS] Record

--- src/lib/typings/utility.ts
+++ src/lib/typings/utility.ts
@@ -1,5 +1,6 @@
 export const utility = {};
 
+// 1. Partial
 type Info = {
   email: string;
   address: string;
@@ -9,3 +10,14 @@ type MyInfo = Partial<Info>;
 const yena: MyInfo = { address: 'abcd', email: 'hey' };
 const gildong: MyInfo = { address: 'abcd' };
 const koko: MyInfo = {};
+
+// 2. Record
+type Animal = {
+  age: number;
+};
+type CatName = 'kitty' | 'mong';
+
+const cats: Record<CatName, Animal> = {
+  kitty: { age: 1 },
+  mong: { age: 2 },
+};

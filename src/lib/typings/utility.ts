42e7539ca6f95912ab9598c5fb740bfb3fe0162a
Yena Hwang <hwangyena@sk.com>
2022-06-09 14:04:24 +0900

    [TS] exclude, extract

--- src/lib/typings/utility.ts
+++ src/lib/typings/utility.ts
@@ -21,3 +21,15 @@ const cats: Record<CatName, Animal> = {
   kitty: { age: 1 },
   mong: { age: 2 },
 };
+
+// 3. Exclude
+type Fruits = 'Apple' | 'Banana' | 'Lemon';
+type YellowFruits = Exclude<Fruits, 'Apple'>;
+
+const yellowFruits: YellowFruits = 'Lemon';
+
+// 4. Extract
+type Color = 'red' | 'orange' | 'green' | 'blue' | 'purple';
+type SKColor = Extract<Color, 'red' | 'orange'>;
+
+const skIcon: SKColor = 'red';
